dd3269c7f1724fa0fa4ad3a8af1e16ed9038fd82
Warren Zhang <warrenzhang17@gmail.com>
2023-04-24 17:57:42 -0400

    navbar and search, filter tbd

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import './index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 
 function Root() {
-  return(
+  return (
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  )
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+render(
   <React.StrictMode>
     <Root />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
