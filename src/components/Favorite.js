b3c46ece06d1bbfb5af9dbafb49d8df797f39870
Chen <chen.jiang.1206@gmail.com>
2023-04-25 16:59:59 -0400

    add FavoriteContext

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,16 +1,21 @@
 
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { FavoriteContext } from "../context/FavoriteContext";
+
 
 function Favorite(props) {
+
+    const { deleteFav } = useContext(FavoriteContext)
+
     const { fav } = props
-    console.log(fav)
 
     return (
         <div>
             <h2>{fav.name}</h2>
             <img src={fav.image} style={{width: 300}}></img>
             <Link to={`/breeds/` + fav.breedId}>See breed detail</Link>
-            <button>remove from favorites</button>
+            <button onClick={()=>deleteFav(fav.id)}>remove from favorites</button>
         </div>
     )
 }
---
+++ src/context/FavoriteContext.js
@@ -0,0 +1,50 @@
+import { createContext, useState } from 'react';
+
+export const FavoriteContext = createContext();
+
+export const FavoriteProvider = props => {
+    const [favorites, setFavorites] = useState([]);
+    const favoriteUrl = "http://localhost:8000/favBreeds/"
+
+    function addFavoriteBreeds(breed, imgByBreedUrl) {
+        //NEED TO ADD CONDITION TO PREVENT REPEATING BREEDS!!
+        const favObj = {
+            breedId: breed.id,
+            name: breed.name,
+            image: imgByBreedUrl
+        }
+
+        const postRequest = {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(favObj)
+        }
+        fetch(favoriteUrl, postRequest)
+            .then(r => r.json())
+            .then(data => setFavorites([...favorites, data]))
+    }
+
+
+    function deleteFav(deletedId) {
+        fetch(favoriteUrl + deletedId, {
+            method: "DELETE",
+        })
+            .then((r) => {
+                if (r.ok) {
+                    setFavorites(favorites.filter((fav) => fav.id !== deletedId))
+                }
+            })
+            .catch((error) => console.error(error))
+    }
+
+    return (
+        <FavoriteContext.Provider value={{ favorites, setFavorites, favoriteUrl, addFavoriteBreeds, deleteFav }}>
+            {props.children}
+        </FavoriteContext.Provider>
+    );
+};
+
+export default FavoriteProvider;
