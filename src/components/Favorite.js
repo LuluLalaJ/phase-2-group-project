0a08aff0122b3401655a640f29cb92d3513952a0
Chen <chen.jiang.1206@gmail.com>
2023-04-25 10:38:07 -0400

    add fav card

---
+++ src/components/Favorite.js
@@ -0,0 +1,17 @@
+
+import { Link } from 'react-router-dom'
+
+function Favorite(props) {
+    const { fav } = props
+    console.log(fav)
+
+    return (
+        <div>
+            <h2>{fav.name}</h2>
+            <img src={fav.image} style={{width: 300}}></img>
+            <button>remove from favorites</button>
+        </div>
+    )
+}
+
+export default Favorite
