21da66dbfce81da552a67bf2bf013f0ecffc55c1
Chen <chen.jiang.1206@gmail.com>
2023-04-25 09:55:39 -0400

    add fav card

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,9 +1,26 @@
-// this is where the favorite button functionality will go
-// GET & POST also occurs here
+//run  json-server --watch db.json --port 8000
+
+import { useEffect, useState } from 'react'
 
 function Favorites() {
-    return (
-      <h1>HELLO FROM Favorites</h1>
+  const [favs, setFavs] = useState([])
+  const favUrl = "http://localhost:8000/favBreeds"
+
+  useEffect(() => {
+    fetch(favUrl)
+    .then(r => r.json())
+    .then(data => setFav(data))
+  },[])
+
+  const renderFavs = favs.map( fav => <)
+
+
+  return (
+      <div>
+        <h1>Here are your favorite cats</h1>
+        {renderFavs}
+      </div>
+
     );
   }
 
